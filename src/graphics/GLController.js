9c2ea9660058a2d191b494511e3b83b4933d4a75
safeer <safeer2c@gmail.com>
2018-06-04 05:38:06 +0530

    Refactoring work

---
+++ src/graphics/GLController.js
@@ -0,0 +1,53 @@
+import SceneBuilder from "./SceneBuilder";
+
+export default class GLController {
+  constructor(baseDiv, wrapperDiv) {
+    // List of views we are going to build
+    this.viewList = [];
+    this.viewList.push(new SceneBuilder(wrapperDiv));
+  }
+
+  init(dataObj) {
+    if (!dataObj) return;
+
+    // window.onwheel = function(){ return false; }
+
+    this.dataObject = dataObj;
+
+    this.viewList.forEach(view => {
+      // set params
+      view.dataObject = this.dataObject;
+      view.renderAll = this.renderAll.bind(this);
+      view.createScene();
+    });
+
+    this.renderAll();
+  }
+
+  renderAll() {
+    this.viewList.forEach(view => {
+      view.renderScene();
+    });
+  }
+
+  clearAll() {
+    this.viewList.forEach(view => {
+      view.clear();
+    });
+  }
+
+  onResize(done) {
+    this.viewList.forEach(view => {
+      view.onResize();
+      view.createScene();
+    });
+    this.renderAll();
+    if (done) done();
+  }
+
+  setStateUpdateHandler(stateUpdateHandler) {
+    this.viewList.forEach(view => {
+      view.stateUpdateHandler = stateUpdateHandler;
+    });
+  }
+}
--- src/graphics/lib/GraphicsLayer.js
+++ src/graphics/lib/GraphicsLayer.js
@@ -1,42 +1,46 @@
-import { Canvas2D } from '../ShaderFactory/Canvas'
+import { Canvas2D, Canvas } from "../ShaderFactory/Canvas";
 
 // GraphicsLayer Layer
 export default class GraphicsLayer {
+  // Construct canvas and webgl context
+  constructor(wrapperElem) {
+    if (!wrapperElem) console.error("Canvas Wrapper Element is unset");
 
-    // Construct canvas and webgl context
-    constructor(wrapperElem, canvasObj) {
-        if (!wrapperElem) console.error("Canvas Wrapper Element is unset");
+    this.wrapperElem = wrapperElem;
 
-        this.wrapperElem = wrapperElem;
+    const canvasObj = new Canvas(wrapperElem);
+    this.canvas = canvasObj.canvas;
+    this.gl = canvasObj.gl;
+    this.shaderFac = canvasObj.shaderFac;
+  }
 
-        this.canvas = canvasObj.canvas;
-        this.gl = canvasObj.gl;
-        this.shaderFac = canvasObj.shaderFac;
+  clear() {
+    const { gl } = this;
+    if (gl) {
+      gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
     }
-
-    clear() {
-        var gl = this.gl;
-        if (gl) {
-            gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
-        }
-        if (this.canvas2D) {
-            this.canvas2D.ctx.clearRect(0, 0, this.canvas2D.canvas.width, this.canvas2D.canvas.height);
-        }
+    if (this.canvas2D) {
+      this.canvas2D.ctx.clearRect(
+        0,
+        0,
+        this.canvas2D.canvas.width,
+        this.canvas2D.canvas.height
+      );
     }
+  }
 
-    onResize()
-    {
-        this.clear();
-        this.canvas.width = this.wrapperElem.clientWidth;
-        this.canvas.height = this.wrapperElem.clientHeight;
-        this.gl.viewport(0, 0, this.canvas.width, this.canvas.height);
-        if(this.canvas2D) {
-            this.canvas2D.canvas.width = this.wrapperElem.clientWidth;
-            this.canvas2D.canvas.height = this.wrapperElem.clientHeight;
-        }
+  onResize() {
+    this.clear();
+    this.canvas.width = this.wrapperElem.clientWidth;
+    this.canvas.height = this.wrapperElem.clientHeight;
+    this.gl.viewport(0, 0, this.canvas.width, this.canvas.height);
+    if (this.canvas2D) {
+      this.canvas2D.canvas.width = this.wrapperElem.clientWidth;
+      this.canvas2D.canvas.height = this.wrapperElem.clientHeight;
     }
+  }
 
-    addCanvas2D() {
-        this.canvas2D = new Canvas2D(this.wrapperElem);
-    }
+  addCanvas2D() {
+    this.canvas2D = new Canvas2D(this.wrapperElem);
+  }
 }
