c0242bee152803735728ab8ca7ad33db157002f2
safeer <safeer2c@gmail.com>
2018-06-22 15:16:08 +0530

    Changes to Custom Popover styles

--- src/components/Overlay/CustomPopover.js
+++ src/components/Overlay/CustomPopover.js
@@ -4,10 +4,10 @@ export default class CustomPopover extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      position: {y: 500}
-    }
+      position: { y: 500 }
+    };
   }
-  
+
   render() {
     if (!this.props.visible) return null;
 
@@ -23,8 +23,8 @@ export default class CustomPopover extends React.Component {
         <div>
           {this.props.displayItemList.map(displayItem => {
             let { text, key } = displayItem;
-            if (text.length > 25) text = text.substr(0, 25) + "..";
-            return <h4 key={key}>{text}</h4>;
+            if (text.length > 25) text = `${text.substr(0, 50)}..`;
+            return <p key={key}>{text}</p>;
           })}
         </div>
       </div>
